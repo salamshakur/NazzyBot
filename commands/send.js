afe3d738a65a3d06cff7e5071ca46e6aee67e577
salamshakur <salamshakur@knights.ucf.edu>
2018-10-23 18:28:05 -0400

    Commit #2 - Bot sends random cat pictures! Meow!

---
+++ commands/send.js
@@ -0,0 +1,18 @@
+const snekfetch = require('snekfetch');
+
+module.exports = {
+	name: 'send',
+	description: 'Ask me to send you anything! For example: Say !send cats',
+	async execute(message, args) {
+
+		if(!args.length) {
+			return message.channel.send('You need to supply a send item!');
+		}
+
+		if(args[0] === 'cats') {
+			const { body } = await snekfetch.get('https://aws.random.cat/meow');
+			message.channel.send(body.file);
+		}
+
+	},
+};
