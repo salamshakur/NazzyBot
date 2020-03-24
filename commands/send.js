07b1d59d49178e85459b70f5faee08cce2cc80cf
Abdool Shakur <salamshakur@knights.ucf.edu>
2020-03-24 03:27:24 -0400

    can now search anything!

--- commands/send.js
+++ commands/send.js
@@ -1,12 +1,20 @@
 const snekfetch = require('snekfetch');
+var cheerio = require("cheerio");
+var request = require("request");
 
 module.exports = {
 	name: 'send',
 	description: 'Ask me to send you anything! For example: Say !send cats',
 	async execute(message, args) {
 
+		var search;
+
 		if(!args.length) {
 			return message.channel.send('You need to supply a send item!');
+		}
+		else
+		{
+			search = args.join(' ');
 		}
 
 		if(args[0] === 'cats') {
@@ -14,10 +22,40 @@ module.exports = {
 			message.channel.send(body.file);
 		}
 
-		if(args[0] === 'dogs'){
+		else if(args[0] === 'dogs') {
 			const { body } = await snekfetch.get('https://dog.ceo/api/breeds/image/random');
 			message.channel.send(body.message);
 		}
+		
+		else {
+			var options = {
+				url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+				method: "GET",
+				headers: {
+					"Accept": "text/html",
+					"User-Agent": "Chrome"
+				}
+			};
 
+			request(options, function(error, response, responseBody) {
+
+				if (error) {
+					return;
+				}
+		 
+				$ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
+		 
+				var links = $(".image a.link");
+
+				var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+
+				if (!urls.length) {
+					return;
+				}
+		 
+				message.channel.send( urls[0] );
+
+			});
+		}
 	},
 };
